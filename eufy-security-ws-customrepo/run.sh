60a36d2287b19519f4e0b95b33847f9175239f68
genomez <genomez@users.noreply.github.com>
2026-08-30 15:08:17 -0500

    Package FR RTC signaling host test

--- eufy-security-ws-customrepo/run.sh
+++ eufy-security-ws-customrepo/run.sh
@@ -58,6 +58,7 @@ bashio::log.info "TEST_PROVENANCE resolved ws_entry=${WS_ENTRY} client_entry=${C
 check_provenance_marker "ws_post_connect_fallback" "$WS_SERVER" "post-connect-fallback"
 check_provenance_marker "ws_mega_bootstrap" "$WS_MEGA_LOGIN" "v6 mega login: bootstrap triggered"
 check_provenance_marker "client_rtc_context" "$CLIENT_RTC_SIGNALING" "RtcSignaling fetchSign context"
+check_provenance_marker "client_rtc_eu_host" "$CLIENT_RTC_SIGNALING" "security-smart-eu.eufylife.com"
 check_provenance_marker "client_mega_lifecycle_queued" "$CLIENT_MEGA_API" "MegaApi lifecycle queued"
 check_provenance_marker "client_mega_lifecycle_started" "$CLIENT_MEGA_API" "MegaApi lifecycle HTTP started"
 check_provenance_marker "client_mega_lifecycle_timeout" "$CLIENT_MEGA_API" "MEGA_REQUEST_LIFECYCLE_TIMEOUT"
